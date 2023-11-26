bcf477f0f274eb82794cbb727321770abed06903
Bjartur <bjarturluk@gmail.com>
2023-11-26 22:18:02 +0100

    refactoring

--- apk.sh
+++ apk.sh
@@ -67,6 +67,7 @@ leak_check() {
 }
 
 sign() {
+    rm $APK_NAME.keystore
     # Generate the keystore
     keytool -genkey -v -keystore $APK_NAME.keystore -alias $APK_NAME -keyalg RSA -keysize 2048 -validity 10000 -storepass 123123 -keypass 123123 -dname "cn=Unknown, ou=Unknown, o=Unknown, c=Unknown"
 
@@ -82,7 +83,7 @@ apktool_assemble() {
     apktool b $APK_NAME
 }
 
-jadx() {
+jadx_convert() {
     echo -e "Initializing JADX conversion process..."
     APK_NAME=kringlan
     for dir in "$APK_NAME"/smali*; do
@@ -99,7 +100,7 @@ jadx() {
 
 java_handler() {
     sh $HOME/netsec/android/tools/dex2jar/d2j-dex2jar.sh -f $APK_FILE_NAME -o $APK_NAME.jar
-    echo "${GREEN}Please select: File > Save All Sources in JD-GUI. Then close the program${NC}"
+    echo -e "${GREEN}Please select: File > Save All Sources in JD-GUI. Then close the program${NC}"
     java -jar $HOME/netsec/android/tools/jd-gui-1.6.6.jar $APK_NAME.jar
     unzip $APK_NAME.jar.src.zip -d $APK_NAME-java
 }
@@ -243,9 +244,7 @@ frida_run() {
 }
 
 create_frida_script() {
-    echo "Dumping available classes to frida_class_dump.txt..."
-    timeout 10s frida -U -f com.hacker101.webdev -e "Java.perform(function () { Java.enumerateLoadedClasses({ 'onMatch': function (className) { console.log(className) }, 'onComplete': function () { } }) })" >output.txt
-    echo "frida -U -f com.hacker101.webdev -l $(pwd)/frida_script.js" | pbcopy
+    echo "frida -U -f $APK_PACKAGE_NAME -l $(pwd)/frida_script.js" | pbcopy
     cat <<EOF >frida_script.js
 Java.perform(function () {
     /*
@@ -254,18 +253,23 @@ Java.perform(function () {
     in a new terminal with Frida running.
     Cheat sheet: https://appsec-labs.com/portal/frida-cheatsheet-for-android/
     Examples: https://github.com/iddoeldor/frida-snippets#binder-transactions
+    Run this to get an overview of loaded classes:
+    timeout 10s frida -U -f $APK_PACKAGE_NAME -e "Java.perform(function () { Java.enumerateLoadedClasses({ 'onMatch': function (className) { console.log(className) }, 'onComplete': function () { } }) })" >frida_class_dump.txt
     */
     var MainActivity = Java.use("$APK_PACKAGE_NAME.MainActivity");
 
     // This can be any method you want
-    MainActivity.onCreate.implementation = function (view) {
+    MainActivity.onBackPressed.implementation = function () {
+        try {
         // Log the method call
-        console.log('onCreate called with view: ' + view);
-
-        // Call the original onCreate method
-        this.onCreate(view);
-
+        console.log("MainActivity.onBackPressed called");
+        // Call the original onBackPressed method
+        this.onBackPressed();
         // Perform any additional actions here
+        } catch (err) {
+            console.log("Error in MainActivity.onBackPressed");
+            console.log(err);
+        }
     };
 });
 EOF
@@ -279,14 +283,29 @@ apktool_disassemble() {
     apktool d $APK_FILE_NAME
 }
 
+get_logs() {
+    adb logcat | grep "$APK_PACKAGE_NAME"
+}
+
 main() {
     clear
     echo -e "${GREEN}---------------------------------------------------------------------"
     echo -e "Welcome!"
-    echo -e "This script will help you to decompile, recompile, sign and then"
+    echo -e "This script will help you to decompile, recompile, edit, sign and then"
     echo -e "run your APK file."
     echo -e "---------------------------------------------------------------------${NC}"
     init
+    if [ $# -eq 1 ]; then
+        # Check if the provided argument is a valid function
+        if [ "$(type -t "$1")" = "function" ]; then
+            echo -e "${CYAN}Running function: $1${NC}"
+            $1 # Execute the provided function
+            exit 0
+        else
+            echo -e "${RED}Invalid function name: $1${NC}"
+            exit 1
+        fi
+    fi
     read -p "Press enter to initialize the environment..."
     clean
     stop
@@ -305,7 +324,7 @@ main() {
     echo -e "You might need to turn off the Wifi, with no proxy enabled, then turn it back on until you have an active connection."
     echo -e "When you have a connection then you can configure the proxy."
     echo -e "When you are done looking around the app and want to continue, press enter, then Q, then enter again.${NC}"
-    read -p "Do you want to start SSL unpinning? (y/n): " ssl_unpinning
+    read -p "Do you want to perform SSL unpinning? (y/n): " ssl_unpinning
     if [[ "$(echo "$ssl_unpinning" | tr '[:upper:]' '[:lower:]')" == "y" ]]; then
         ssl_unpin
     fi
@@ -316,16 +335,16 @@ main() {
         echo -e "Native app detected. Running the appropriate decompilers..."
         rm -r $APK_NAME
         apktool_disassemble
-        jadx
+        jadx_convert
         hermes_decompiler
-        hermes_disassemble
+        hermes_disassembler
         hermes_file_parser
     else
         java_handler
         echo "You can now check out your decompiled Java classes."
-        read -p "Do you want to start SSL unpinning? (y/n): " ssl_unpinning
-        create_frida_script
-        if [[ "$(echo "$ssl_unpinning" | tr '[:upper:]' '[:lower:]')" == "y" ]]; then
+        read -p "Do you want to do some Frida debugging? (y/n): " frida_debugging
+        if [[ "$(echo "$frida_debugging" | tr '[:upper:]' '[:lower:]')" == "y" ]]; then
+            create_frida_script
             ssl_unpin
         fi
         echo "${RED}Goodbye!${NC}"
@@ -335,7 +354,12 @@ main() {
     echo -e "${YELLOW}You can now inspect the smali files in the $APK_NAME/smali* directories and make changes to them."
     echo -e "You can also take a look in the hermes or jadx folders to get a better understanding of the source code.${NC}"
 
-    read -p "Press enter to start SSL unpinning again to take a look around again."
+    read -p "Do you want to do some Frida debugging? (y/n): " frida_debugging
+    if [[ "$(echo "$frida_debugging" | tr '[:upper:]' '[:lower:]')" == "y" ]]; then
+        create_frida_script
+    fi
+
+    read -p "Press enter to perform SSL unpinning again to take a look around again."
     stop
     ssl_unpin
 
@@ -352,4 +376,8 @@ main() {
     echo "${GREEN}Goodbye${NC}"
 }
 
-main
+if [ "$#" -eq 1 ]; then
+    main "$1"
+else
+    main
+fi
